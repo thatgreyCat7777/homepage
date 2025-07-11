9301e619a8d4baf838b59f927ad855494404458c
C̴̄̂ä̴́͗t̴̓̑ <151648110+thatgreyCat7777@users.noreply.github.com>
2025-07-12 01:12:34 +0800

    Added icon to page and tidied up navbar code

--- navbar.js
+++ navbar.js
@@ -1,17 +1,25 @@
 // Checks which page the user is in and activates the correct page based off it
-document.addEventListener("DOMContentLoaded", function() {
-    var nav = document.querySelectorAll("#nav li a");
-    for (let i = 0; i < nav.length; i++) {
-        if (document.URL.includes(nav[i].getAttribute("href"))) {
-            nav[i].classList.add("active");
-        } else {
-            nav[i].classList.remove("active");
-        }
-        nav[i].addEventListener('click', function() {
-            for (var j = 0; j < nav.length; j++) {
-                nav[j].classList.remove("active");
+document.addEventListener("DOMContentLoaded", function () {
+    // Loads navbar into page
+    fetch("navbar.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.querySelector("#navbar-placeholder").innerHTML = data;
+        })
+        .then((data) => {
+            var nav = document.querySelectorAll("#nav li a");
+            for (let i = 0; i < nav.length; i++) {
+                if (document.URL.includes(nav[i].getAttribute("href"))) {
+                    nav[i].classList.add("active");
+                } else {
+                    nav[i].classList.remove("active");
+                }
+                nav[i].addEventListener("click", function () {
+                    for (var j = 0; j < nav.length; j++) {
+                        nav[j].classList.remove("active");
+                    }
+                    this.classList.add("active");
+                });
             }
-            this.classList.add("active");
         });
-    }
 });
