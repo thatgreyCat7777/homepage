95bab1c69d217a48861f9c67f0174f370f8de37d
thatgreyCat7777 <devonlim.2009@gmail.com>
2025-07-13 00:12:40 +0800

    Updated portfolio website to more recent status

--- slides.js
+++ slides.js
@@ -11,19 +11,19 @@ function autoClickButton() {
             return autoClickButton();
         }
         window.onload = function () {
-            var interval = autoClickButton();
-            var sliders = document.querySelectorAll("#sliders button");
-            var buttons = document.querySelectorAll("#carouselExampleCaptions button");
-            for (var i = 0, len = sliders.length; i < len; i++) {
-                sliders[i].addEventListener('click', function () {
-                    interval = resetInterval(interval);
-                });
-            }
-            for (var i = 0, len = buttons.length; i < len; i++) {
-                buttons[i].addEventListener('click', function () {
-                    interval = resetInterval(interval);
-                });
-            }
+            // var interval = autoClickButton();
+            // var sliders = document.querySelectorAll("#sliders button");
+            // var buttons = document.querySelectorAll("#carouselExampleCaptions button");
+            // for (var i = 0, len = sliders.length; i < len; i++) {
+            //     sliders[i].addEventListener('click', function () {
+            //         interval = resetInterval(interval);
+            //     });
+            // }
+            // for (var i = 0, len = buttons.length; i < len; i++) {
+            //     buttons[i].addEventListener('click', function () {
+            //         interval = resetInterval(interval);
+            //     });
+            // }
             document.addEventListener('keydown', function (event) {
                 if ([37, 65].includes(event.key)) {
                     document.querySelector('#previous').click();
