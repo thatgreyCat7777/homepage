0a4949fb9b68e25e6739d6ffe8f779d2dd8ee1e0
C̴̄̂ä̴́͗t̴̓̑ <151648110+thatgreyCat7777@users.noreply.github.com>
2026-08-30 16:11:52 +0800

    Changed color of text in slides to be inverted color of background

--- js/slides.js
+++ js/slides.js
@@ -1,8 +1,16 @@
+function playVideo() {
+    var video = document.querySelector(".carousel-item-end video");
+    console.log(video);
+    if (video) {
+        video.currentTime = 0; 
+    }
+}
 function autoClickButton() {
     var nextButton = document.querySelector("#next");
     var myInterval = setInterval(function () {
         nextButton.click();
     }, 8500);
+    playVideo();
     return myInterval;
 }
 
@@ -13,22 +21,27 @@ function resetInterval(interval) {
 window.onload = function () {
     var interval = autoClickButton();
     var sliders = document.querySelectorAll("#sliders button");
-    var buttons = document.querySelectorAll("#carouselExampleCaptions button");
+    var buttons = document.querySelectorAll("#previous, #next");
     for (var i = 0, len = sliders.length; i < len; i++) {
         sliders[i].addEventListener("click", function () {
             interval = resetInterval(interval);
+            playVideo();
         });
     }
+    // TODO - Make play video work when pressing arrow keys to navigate slides
     for (var i = 0, len = buttons.length; i < len; i++) {
         buttons[i].addEventListener("click", function () {
             interval = resetInterval(interval);
+            playVideo();
         });
     }
     document.addEventListener("keydown", function (event) {
         if ([37, 65].includes(event.key)) {
             document.querySelector("#previous").click();
+            playVideo();
         } else if ([39, 68].includes(event.key)) {
             document.querySelector("#next").click();
+            playVideo();
         }
     });
 };
